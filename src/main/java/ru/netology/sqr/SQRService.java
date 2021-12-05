84508a6f00bfe542073bb738d4b152547cf33f7e
Alexander211996 <91474414+Alexander211996@users.noreply.github.com>
2021-12-05 21:21:04 +0300

    Update SQRService.java

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,11 +1,11 @@
 package ru.netology.sqr;
 
 public class SQRService {
-    public int sqrQuantity(int K) {
-        K = 0;
+    public int sqrQuantity(int LowLimit, int UpperLimit) {
+        int K = 0;
         int i = 10;
-        int LowLimit = 200;
-        int UpperLimit = 300;
+        LowLimit = 200;
+        UpperLimit = 300;
 
         for (; i >= 10 && i <= 99; i++) {
             if ((i * i >= LowLimit) && (i * i <= UpperLimit)) {
